a7ac8d40e1e7897c203aae37192594a0bde99d8b
Mike Chung <mikechung@live.com>
2017-09-20 12:01:24 -0700

    add stored procs for Characters table.

---
+++ Database/stored_procs/Character_Delete.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE [dbo].[Character_Delete]
+	@id int
+AS
+/*
+
+DECLARE @_id INT = 9;
+EXEC [Character_Delete]
+	@_id
+SELECT * FROM [dbo].[Characters]
+
+*/
+BEGIN
+	DELETE FROM [dbo].[Characters]
+	WHERE Id = @id
+END

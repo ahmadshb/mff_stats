72a4d884c56f0e4ebcd2d68e453fde2d1771c841
Mike Chung <mikechung@live.com>
2017-09-20 16:16:09 -0700

    Add all stored procs to CharacterService.

--- Services/CharacterService.cs
+++ Services/CharacterService.cs
@@ -1,11 +1,9 @@
-﻿using MFFStats.Models.Requests;
+﻿using MFFStats.Models.Domain;
+using MFFStats.Models.Requests;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MFFStats.Services
 {
@@ -34,6 +32,58 @@ namespace MFFStats.Services
             return id;
         }
 
+        public Character SelectById(int id)
+        {
+            Character model = new Character();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("Character_SelectById", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        model = Mapper(reader);
+                    }
+                }
+                conn.Close();
+            }
+            return model;
+        }
+
+        public List<Character> SelectAll()
+        {
+            List<Character> charList = new List<Character>();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("Character_SelectAll", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                    while (reader.Read())
+                    {
+                        Character model;
+                        try
+                        {
+                            model = Mapper(reader);
+                        }
+                        catch (Exception ex)
+                        {
+                            string msg = ex.Message;
+                            throw;
+                        }
+                        charList.Add(model);
+                    }
+                }
+                conn.Close();
+            }
+            return charList;
+        }
+
         public void Update(CharacterUpdateRequest model)
         {
             using (SqlConnection conn = new SqlConnection(connString))
@@ -49,6 +99,61 @@ namespace MFFStats.Services
                 }
                 conn.Close();
             }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("Character_Delete", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    cmd.ExecuteNonQuery();
+                }
+                conn.Close();
+            }
+        }
+
+        private Character Mapper(SqlDataReader reader)
+        {
+            Character model = new Character();
+            int index = 0;
+            model.Id = reader.GetInt32(index++);
+            model.Name = reader.GetString(index++);
+            model.Type = reader.GetString(index++);
+            model.Species = reader.GetString(index++);
+            model.Gender = reader.GetString(index++);
+            model.Side = reader.GetString(index++);
+            model.Tier2 = reader.GetBoolean(index++);
+            model.PhysicalAtk = reader.GetInt32(index++);
+            model.EnergyAtk = reader.GetInt32(index++);
+            model.AtkSpeed = reader.GetDecimal(index++);
+            model.CritRate = reader.GetDecimal(index++);
+            model.CritDmg = reader.GetDecimal(index++);
+            model.IgnoreDef = reader.GetDecimal(index++);
+            model.IgnoreDodge = reader.GetDecimal(index++);
+            model.FireDmg = reader.GetDecimal(index++);
+            model.ColdDmg = reader.GetDecimal(index++);
+            model.LightningDmg = reader.GetDecimal(index++);
+            model.PoisonDmg = reader.GetDecimal(index++);
+            model.MindDmg = reader.GetDecimal(index++);
+            model.PhysicalDef = reader.GetInt32(index++);
+            model.EnergyDef = reader.GetInt32(index++);
+            model.HP = reader.GetInt32(index++);
+            model.RecoveryRate = reader.GetDecimal(index++);
+            model.Dodge = reader.GetDecimal(index++);
+            model.FireResist = reader.GetDecimal(index++);
+            model.ColdResist = reader.GetDecimal(index++);
+            model.LightningResist = reader.GetDecimal(index++);
+            model.PoisonResist = reader.GetDecimal(index++);
+            model.MindResist = reader.GetDecimal(index++);
+            model.MoveSpd = reader.GetDecimal(index++);
+            model.Debuff = reader.GetDecimal(index++);
+            model.SkillCooldown = reader.GetDecimal(index++);
+
+            return model;
         }
 
         private static void AddValues(CharacterAddRequest model, SqlCommand cmd)

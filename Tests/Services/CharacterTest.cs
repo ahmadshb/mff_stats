0fa95ba60494b8b91a97982c1517cc8d3a97a03b
Mike Chung <mikechung@live.com>
2017-09-20 17:00:20 -0700

    Add Tests project for unit testing.  Add App.Config for unit test connection string.  Test Select All.

---
+++ Tests/Services/CharacterTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MFFStats.Services;
+using MFFStats.Models.Domain;
+using System.Collections.Generic;
+
+namespace MFFStats.Tests.Services
+{
+    [TestClass]
+    public class CharacterTest
+    {
+        [TestMethod]
+        public void CharacterService_SelectAll_Test()
+        {
+            CharacterService svc = new CharacterService();
+            List<Character> modelList = svc.SelectAll();
+            Assert.IsNotNull(modelList);
+
+        }
+    }
+}

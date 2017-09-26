17bcf7c499a9e94cea07a49145d0730faf15f1cb
Mike Chung <mikechung@live.com>
2017-09-26 14:31:04 -0700

    Add admin area to project.  Add CharacterController and view to admin area.

---
+++ Web/Areas/Admin/Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MFFStats.Web.Areas.Admin.Controllers
+{
+    public class CharacterController : Controller
+    {
+        // GET: Admin/Character
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
